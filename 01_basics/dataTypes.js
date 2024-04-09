12447ddd178a9de7f82059a4f1d0b3638123cdaf
Mudit Mathur <70134440+muditmathurr@users.noreply.github.com>
2024-04-09 08:32:16 +0000

    database types done

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we're using nodejs, not browser
+
+console.log(3 +
+3)  // write code readiable
+
+let name = "Mudit"
+let age = 24
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
