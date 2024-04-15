fe6d105c051adbc309e7fd21ece495dc74994f0a
Mudit Mathur <70134440+muditmathurr@users.noreply.github.com>
2024-04-15 10:26:36 +0000

    functions

---
+++ 03_basics/functions0.js
@@ -0,0 +1,29 @@
+// console.log("M");
+// console.log("U");
+// console.log("D");
+// console.log("I");
+// console.log("T");
+
+// function sayMyName(parameters) {
+//     console.log("M");
+//     console.log("U");
+//     console.log("D");
+//     console.log("I");
+//     console.log("T");
+// }
+
+// sayMyName(arguments)
+
+
+//console
+function addTwoNumber(n1, n2) {
+    console.log(n1 + n2);
+}
+
+// return
+function addTwoNumber(n1, n2) {
+    let result = n1 + n2
+    return result
+}
+const result = addTwoNumber(5, 6)
+console.log("Result: ", result);
