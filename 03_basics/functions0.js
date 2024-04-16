ac58863715bbfa22219deec7978a0af2b8bd971c
Mudit Mathur <70134440+muditmathurr@users.noreply.github.com>
2024-04-16 09:00:07 +0000

    function done

--- 03_basics/functions0.js
+++ 03_basics/functions0.js
@@ -27,3 +27,53 @@ function addTwoNumber(n1, n2) {
 }
 const result = addTwoNumber(5, 6)
 console.log("Result: ", result);
+
+function userLogIn(username) {
+    return `${username} just logged in`
+}
+
+// console.log(userLogIn("MUDIT"));
+// console.log(userLogIn()); // WILL GIVE undefined
+
+
+// if else
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Mudit"))
+// console.log(loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "mudit",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
