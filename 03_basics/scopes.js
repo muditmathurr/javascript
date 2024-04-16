0b97dc9e28990930c58a27dfa2cada598f0e3d2e
Mudit Mathur <70134440+muditmathurr@users.noreply.github.com>
2024-04-16 10:34:08 +0000

    scopes  done

---
+++ 03_basics/scopes.js
@@ -0,0 +1,58 @@
+ // { scope }
+
+    // var c = 300
+let a = 300
+
+if(true) {
+    let a = 10 
+    const b = 20
+    var c = 30
+    // console.log("INNER: ", a);
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+    const username = "mudit"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website0);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "mudit"
+    if (username === "mudit") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
